35dde4cd228b43d0b781aa6f333dc3f5ebe31429
corgifist <86985149+corgifist@users.noreply.github.com>
2023-03-05 22:17:44 +0300

    Added basic parser, bytecode implementation and disassembler

--- SPS3/src/main/java/com/timsystem/spk/Main.java
+++ SPS3/src/main/java/com/timsystem/spk/Main.java
@@ -1,21 +1,26 @@
 package com.timsystem.spk;
 
+import com.timsystem.spk.compiler.Lexer;
+import com.timsystem.spk.compiler.Parser;
+import com.timsystem.spk.compiler.ast.AST;
+import com.timsystem.spk.compiler.ast.BinaryAST;
+import com.timsystem.spk.compiler.ast.ConstantAST;
+import com.timsystem.spk.compiler.ast.ProgramAST;
+import com.timsystem.spk.compiler.lib.Token;
+import com.timsystem.spk.compiler.lib.TokenType;
 import com.timsystem.spk.vm.Bytecode;
-import com.timsystem.spk.vm.Run;
+import com.timsystem.spk.vm.Disassemble;
+import com.timsystem.spk.vm.Instructions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
-        ArrayList<Byte> bts = new ArrayList<>();
-        bts.add((byte)1);
-        bts.add((byte)0);
-        bts.add((byte)0);
-        Bytecode bytecode = new Bytecode();
-        bytecode.setConstants(new ArrayList<Object>());
-        bytecode.setBytecode(bts);
-        bytecode.getConstants().add(0);
-        new Run(bytecode).run();
+        Bytecode bc = new Bytecode();
+        Parser parser = new Parser(new Lexer("1 + 1 + 1"));
+        AST ast = parser.parse();
+        ast.compile(bc);
+        Disassemble.disassemble("AST test", bc);
     }
 }
---
+++ SPS3/src/main/java/com/timsystem/spk/vm/Instructions.java
@@ -0,0 +1,17 @@
+package com.timsystem.spk.vm;
+
+public class Instructions {
+
+    public static final byte OP_PUSH = 1,
+                        OP_DUP = 2,
+                        OP_POP = 3,
+                        OP_FRAME = 4,
+                        OP_FLIP = 5,
+                        OP_SWAP = 6,
+                        OP_OUT = 7,
+                        OP_INP = 8,
+                        OP_HALT = 9,
+                        OP_BINARY = 10,
+                        OP_NEGATE = 11;
+
+}
