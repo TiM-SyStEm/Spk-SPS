0e816c5dca18ff030448788bc8704f3bf161e123
Timofei Gorlov <72683799+TiM-SyStEm@users.noreply.github.com>
2023-04-25 19:55:58 +0300

    jump instes#2

--- SPS3/src/main/java/com/timsystem/spk/Main.java
+++ SPS3/src/main/java/com/timsystem/spk/Main.java
@@ -11,16 +11,22 @@ import com.timsystem.spk.compiler.lib.TokenType;
 import com.timsystem.spk.vm.Bytecode;
 import com.timsystem.spk.vm.Disassemble;
 import com.timsystem.spk.vm.Instructions;
+import com.timsystem.spk.vm.Run;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Bytecode bc = new Bytecode();
+        /*Bytecode bc = new Bytecode();
         Parser parser = new Parser(new Lexer("var abc = 2 + 2 var abcd = abc + 1"));
         AST ast = parser.parse();
         ast.compile(bc);
-        Disassemble.disassemble("AST test", bc);
+        Disassemble.disassemble("AST test", bc);*/
+        Bytecode bc = new Bytecode();
+
+        Run run = new Run(bc);
+        run.run();
+        //Disassemble.disassemble("AST test", bc);
     }
 }
