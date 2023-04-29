2ebbda73a4a99b920688ad3e67cc29e759a550aa
corgifist <86985149+corgifist@users.noreply.github.com>
2023-04-29 15:41:35 +0300

    Disassembler enhancements

--- SPS3/src/main/java/com/timsystem/spk/vm/Disassemble.java
+++ SPS3/src/main/java/com/timsystem/spk/vm/Disassemble.java
@@ -17,7 +17,7 @@ public class Disassemble {
             System.out.printf("%04d %s ", i, line == previousLine ? "|" : line);
             switch (instruction) {
                 case Instructions.OP_PUSH -> {
-                    byte[] indexBytes = new byte[] {
+                    byte[] indexBytes = new byte[]{
                             bytes.get(i + 1),
                             bytes.get(i + 2),
                             bytes.get(i + 3),
@@ -58,7 +58,7 @@ public class Disassemble {
                     immediateInstruction("OP_NEGATE");
                 }
                 case Instructions.OP_GET_VAR -> {
-                    byte[] indexBytes = new byte[] {
+                    byte[] indexBytes = new byte[]{
                             bytes.get(i + 1),
                             bytes.get(i + 2),
                             bytes.get(i + 3),
@@ -68,7 +68,7 @@ public class Disassemble {
                     constantInstruction("OP_GET_VAR", bytecode, IntegerBytesConvert.byteArr2Int(indexBytes));
                 }
                 case Instructions.OP_CREATE_VAR -> {
-                    byte[] indexBytes = new byte[] {
+                    byte[] indexBytes = new byte[]{
                             bytes.get(i + 1),
                             bytes.get(i + 2),
                             bytes.get(i + 3),
@@ -125,6 +125,33 @@ public class Disassemble {
                     jumpInstruction("OP_LOOP", bytecode.getBytecode(), i);
                     i += 4;
                 }
+                case Instructions.OP_CALL -> {
+                    jumpInstruction("OP_CALL", bytecode.getBytecode(), i);
+                    i += 4;
+                }
+                case Instructions.OP_RET -> immediateInstruction("OP_RET");
+                case Instructions.OP_AND -> immediateInstruction("OP_AND");
+                case Instructions.OP_OR -> immediateInstruction("OP_OR");
+                case Instructions.OP_XOR -> immediateInstruction("OP_XOR");
+                case Instructions.OP_NOT -> immediateInstruction("OP_NOT");
+                case Instructions.OP_JIT -> {
+                    jumpInstruction("OP_JIT", bytecode.getBytecode(), i);
+                    i += 4;
+                }
+                case Instructions.OP_JIF -> {
+                    jumpInstruction("OP_JIF", bytecode.getBytecode(), i);
+                    i += 4;
+                }
+                case Instructions.OP_FRGET -> {
+                    constantInstruction("OP_FRGET", bytecode, i);
+                    i += 4;
+                }
+                case Instructions.OP_CLR -> {
+                    immediateInstruction("OP_CLR");
+                }
+                case Instructions.OP_PROC -> {
+                    // why?
+                }
                 default -> {
                     System.out.printf("NO SPECIFICATION FOR " + instruction + " OPCODE!\n");
                 }
@@ -134,7 +161,7 @@ public class Disassemble {
     }
 
     private static void jumpInstruction(String name, ArrayList<Byte> bytes, int i) {
-        byte[] indexBytes = new byte[] {
+        byte[] indexBytes = new byte[]{
                 bytes.get(i + 1),
                 bytes.get(i + 2),
                 bytes.get(i + 3),
@@ -148,9 +175,11 @@ public class Disassemble {
         char operation = (char) (bytecode.getBytecode().get(index + 1) & 0xFF);
         System.out.printf("%s '%s'\n", name, String.valueOf(operation));
     }
+
     private static void immediateInstruction(String name) {
         System.out.printf("%s\n", name);
     }
+
     private static void constantInstruction(String name, Bytecode bytecode, int index) {
         Object constant = bytecode.getConstants().get(index);
         System.out.printf("%s %s %s %s\n", name, constant.toString(), constant.getClass().getSimpleName(), String.valueOf(index));
